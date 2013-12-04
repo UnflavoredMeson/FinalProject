0bb63366d579ecebf1f5f2e8c127acccf33b1e24
UnflavoredMeson <Smitty7887@gmail.com>
2013-12-04 14:32:12 -0500

    User Page setup

--- app/Global.java
+++ app/Global.java
@@ -1,24 +1,35 @@
 import play.*;
 import play.libs.*;
 
-import com.avaje.ebean.Ebean;
+import java.util.*;
+
+import com.avaje.ebean.*;
 
 import models.*;
 
-import java.util.*;
-
 public class Global extends GlobalSettings {
-    public static void insert(Application app) {
-        if(Ebean.find(User.class).findRowCount() == 0) {
-            
-            @SuppressWarnings("unchecked")
-							Map<String,List<Object>> all = (Map<String,List<Object>>)Yaml.load("initial-data.yml");
-
-            // Insert users first
-            Ebean.save(all.get("users"));
-
-            // Insert projects
-            Ebean.save(all.get("questions"));
-        }
+    
+    public void onStart(Application app) {
+        InitialData.insert(app);
     }
+    
+    static class InitialData {
+        
+        public static void insert(Application app) {
+            if(Ebean.find(User.class).findRowCount() == 0) {
+                
+                @SuppressWarnings("unchecked")
+								Map<String,List<Object>> all = (Map<String,List<Object>>)Yaml.load("initial-data.yml");
+
+                // Insert users first
+                Ebean.save(all.get("users"));
+
+                // Insert projects
+                Ebean.save(all.get("questions"));
+                
+            }
+        }
+        
+    }
+    
 }
